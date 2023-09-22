a63c1a101679676803df49269e8f6ecad0116f11
Zawahir <139978657+MuhammadZawahir@users.noreply.github.com>
2023-09-22 16:16:13 +0500

    Exercise 9

---
+++ index.ts
@@ -0,0 +1,7 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+
+
+let favNumber:number=9;
+console.log(`My favorite number is ${favNumber} because it is my birthday.`);
